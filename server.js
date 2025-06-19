fcd81bcbe47ea2e460d153cf8410118b2faa6b22
Prashant Dubey <erne57874@gmail.com>
2025-06-19 21:33:48 +0530

    adding cors fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 connectDB();
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://emi-management-system.netlify.app/',
   credentials: true,
 }))
 app.use(cookieparser());
